88c81a7a429c0840fc4b95709cedcb2f470342a7
Aleksandr <tsybulskialex@gmail.com>
2024-12-23 15:44:59 +0300

    feat: completed task - dns-stars

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const splitArr = domains.map((item) => item.split('.'));
+  const revArr = splitArr.map(item => item.reverse());
+  let arr = [];
+  for (let i = 0; i < revArr.length; i += 1) {
+    let combi;
+    for (let j = 0; j < revArr[i].length; j += 1) {
+      let cur = revArr[i][j];
+      if (j < 1) {
+        arr.push(`.${cur}`);
+        continue;
+      }
+      if ( j === 1) {
+        combi = `${arr[j-1]}.${cur}`;
+        arr.push(combi);
+      }
+      if (j > 1) {
+        combi = `${arr[j-1]}.${cur}`;
+        arr.push(combi);
+      }
+    }
+  }
+  let count = 0;
+  let obj = {};
+  for (let i of arr) {
+    count = 0;
+    if (obj.hasOwnProperty(i) === false) {
+      count += 1;
+      obj[i] = count;
+    } else {
+      count += 1;
+      obj[i] = +obj[i] + 1;
+    }
+  }
+  return obj;
 }
 
 module.exports = {
